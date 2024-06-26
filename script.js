474b3d027f79a591688c9dc98ca80b30b5e7dc74
JuneNguyen <156218862+NgocDungNguyen@users.noreply.github.com>
2024-06-26 18:27:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     const seeAllAwardsBtn = document.getElementById("see-all-awards-btn");
     const seeAllCertsBtn = document.getElementById("see-all-certs-btn");
